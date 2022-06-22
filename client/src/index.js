1e0bd724b5756a769540616f15a1ad6ead46b357
Ammar Ahmed <ammar@ammar.lan>
2022-06-22 04:19:11 -0400

    CLDFLR-8 set up providers (apollo and chakra)

--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from "@chakra-ui/react"
+import { ApolloProvider, ApolloClient, InMemoryCache, HttpLink } from "@apollo/client"
+
+
+const link = new HttpLink({
+  uri: process.env.NODE_ENV === 'development' ? "http://localhost:8080/graphql" : "prodlink"
+})
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </ApolloProvider>
+    
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
